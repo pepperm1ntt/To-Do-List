f7b5ae3b72a7e93d17cd44e6365b1a1264fae4c7
Nico Kindler <kindlernico@web.de>
2024-09-05 17:04:25 +0200

    finished renderItems function

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-let items = [];
+let items = ["hello world", "123"];
+
+const itemsDiv = document.getElementById("listWindow");
 
 function loadItems() {
 
 }
 
 function renderItems() {
+    itemsDiv.innerHTML = null;
 
+    for (const [idx, item] of Object.entries(items)) {
+        const container = document.createElement("div");
+        container.style.marginBottom = "10px"
+        
+        const text = document.createElement("p");
+        text.style.display = "inline";
+        text.style.marginRight = "10px";
+        text.textContent = item;
+
+        const button = document.createElement("button");
+        button.textContent = "Delete";
+        button.onclick = () => removeItem(idx);
+
+        container.appendChild(text);
+        container.appendChild(button);
+
+        itemsDiv.appendChild(container);
+    }
 }
 
 function saveItems() {
@@ -16,6 +37,6 @@ function addItem() {
 
 }
 
-function removeItem() {
+function removeItem(idx) {
 
 }
